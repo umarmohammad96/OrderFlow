9caf34496393df4cc3f82d4fd0c788c5008fcbbb
umarmohammad96 <onesownumar@gmail.com>
2026-09-18 16:07:35 +0530

    chore: initialize OrderFlow backend

---
+++ src/test/java/com/project/orderflow/OrderflowApplicationTests.java
@@ -0,0 +1,16 @@
+package com.project.orderflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderflowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+}
